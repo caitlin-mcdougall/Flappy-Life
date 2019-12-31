b94032bce4ec38dd1ef13983ac349de3f3bbfaa7
caitlin-mcdougall <41774257+caitlin-mcdougall@users.noreply.github.com>
2019-12-31 11:35:02 +0000

    Set up basic functionality of bird

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,63 @@
-var canvas = document.getElementById('gameCanvas');
-var ctx = canvas.getContext('2d');
-ctx.fillStyle="green";
-ctx.fillRect(10,10,500,500);
+var canvas;
+var gameScreen;
+var player;
+
+var SCREENWIDTH;
+var SCREENHEIGHT;
+var PLAYERWIDTH = 20;
+var PLAYERHEIGHT = 20;
+
+var screenX = 10;
+var screenY = 10;
+var playerX = 40;
+var playerY = 40;
+
+var playerSpeed=1;
+var jump = false;
+
+function init(){
+    canvas = document.getElementById('gameCanvas');
+    SCREENHEIGHT = canvas.height;
+    SCREENWIDTH = canvas.width;
+    gameScreen = canvas.getContext('2d');
+    
+    player = canvas.getContext('2d');
+    //setInterval(playerMove, 100); 
+    setInterval(redraw, 100);
+
+    document.onkeydown = playerJump;
+
+}
+
+function clear(c) {
+    c.clearRect(0,0, SCREENWIDTH, SCREENHEIGHT);
+}
+
+function redraw(){
+    clear(gameScreen);
+    clear(player);
+    gameScreen.fillStyle="lightblue";
+    gameScreen.fillRect(screenX,screenY,SCREENWIDTH,SCREENHEIGHT);
+    player.fillStyle="yellow";
+    player.fillRect(playerX, playerY, PLAYERWIDTH,PLAYERHEIGHT);
+    
+}
+
+function playerJump(e){
+    if(e.keyCode == '32'){
+        jump = true;
+    }
+}
+
+function playerMove(){
+    if (jump == true){
+        playerY = playerY - 10 - (3*playerSpeed);
+        jump = false;
+        playerSpeed = 1;
+    }
+    else{
+        playerY = playerY + playerSpeed;
+        playerSpeed = playerSpeed + 2;
+    }
+}
+
